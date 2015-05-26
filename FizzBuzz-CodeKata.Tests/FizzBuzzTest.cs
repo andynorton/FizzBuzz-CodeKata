d5a05c06723be0fc5d7680608df5fc0b5e616595
Andrew Norton <andyjnorton@mac.com>
2015-05-26 19:29:58 +0100

    TDD kata : unit tests added

--- FizzBuzz-CodeKata.Tests/FizzBuzzTest.cs
+++ FizzBuzz-CodeKata.Tests/FizzBuzzTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using FizzBuzz_CodeKata;
 
 
 namespace FizzBuzz_CodeKata.Test
@@ -7,6 +8,36 @@ namespace FizzBuzz_CodeKata.Test
     [TestFixture]
     public class FizzBuzzTest
     {
+        private FizzBuzz fizzBuzz;
+
+        [SetUp]
+        public void Setup()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        [Test]
+        public void Show_number_if_not_multiple_of_3_or_5()
+        {
+            Assert.That(fizzBuzz.DoFizzBuzz(1), Is.EqualTo("1"));
+        }
         
+        [Test]
+        public void Show_fizz_if_multiple_of_3()
+        {
+            Assert.That(fizzBuzz.DoFizzBuzz(3), Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Show_buzz_if_multiple_of_5()
+        {
+            Assert.That(fizzBuzz.DoFizzBuzz(5), Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Show_fizzbuzz_if_multiple_of_both_3_and_5()
+        {
+            Assert.That(fizzBuzz.DoFizzBuzz(15), Is.EqualTo("FizzBuzz"));
+        }
     }
 }
---
+++ FizzBuzz-CodeKata/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz_CodeKata
+{
+    public class FizzBuzz
+    {
+        public string DoFizzBuzz(int number)
+        {
+            if(DivisibleByThree(number) && DivisibleByFive(number))
+            {
+                return "FizzBuzz";
+            }
+            else if (DivisibleByThree(number))
+            {
+                return "Fizz";
+            }
+            else if (DivisibleByFive(number))
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+            
+        }
+
+        private bool DivisibleByFive(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        public bool DivisibleByThree(int number)
+        {
+            //using modulo to figure out multiple
+            return number % 3 == 0;
+        }
+    }
+}
