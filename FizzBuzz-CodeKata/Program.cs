1ed887516732e6cce6620609a3e6ff185a6dff7d
Andrew Norton <andyjnorton@mac.com>
2015-05-26 18:48:07 +0100

    Added basic project layout ready for kata

--- FizzBuzz-CodeKata/Program.cs
+++ FizzBuzz-CodeKata/Program.cs
@@ -10,26 +10,9 @@ namespace FizzBuzz_CodeKata
     {
         static void Main(string[] args)
         {
-            FizzBuzz_FirstIteration();
+           
         }
-        static void FizzBuzz_FirstIteration()
-        {
-            //first iteration, pre-refactor
-            for (int i = 1; i <= 100; i++)
-            {
-                if (i % 3 == 0)
-                    Console.Write("Fizz");
 
-                if (i % 5 == 0)
-                    Console.Write("Buzz");
-
-                if ((i % 3 != 0) && (i % 5 != 0))
-                {
-                    Console.Write(i);
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
+        
     }
 }
