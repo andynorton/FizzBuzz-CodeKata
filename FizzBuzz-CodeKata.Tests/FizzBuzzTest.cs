140fdbe9d440e85e222dee9ce5b8fc9d0609e79f
Andrew Norton <andyjnorton@mac.com>
2015-05-26 19:05:22 +0100

    Added Nunit ready for session - PULL THIS COMMIT TO GET STARTED

---
+++ FizzBuzz-CodeKata.Tests/FizzBuzzTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace FizzBuzz_CodeKata.Test
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        
+    }
+}
--- FizzBuzz-CodeKata.Tests/UnitTest1.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace FizzBuzz_CodeKata.Tests
-{
-    
-}
