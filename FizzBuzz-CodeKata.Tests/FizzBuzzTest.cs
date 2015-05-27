8a35e8498a65fa2816dcb91c19219f9c02d23ec2
Andrew Norton <andyjnorton@mac.com>
2015-05-27 08:25:44 +0100

    Cleaned up unit tests

--- FizzBuzz-CodeKata.Tests/FizzBuzzTest.cs
+++ FizzBuzz-CodeKata.Tests/FizzBuzzTest.cs
@@ -17,25 +17,25 @@ namespace FizzBuzz_CodeKata.Test
         }
 
         [Test]
-        public void Show_number_if_not_multiple_of_3_or_5()
+        public void FizzBuzz_NumberNotMultipleOf3Or5_Return1()
         {
             Assert.That(fizzBuzz.DoFizzBuzz(1), Is.EqualTo("1"));
         }
         
         [Test]
-        public void Show_fizz_if_multiple_of_3()
+        public void FizzBuzz_MultipleOf3_ReturnsFizz()
         {
             Assert.That(fizzBuzz.DoFizzBuzz(3), Is.EqualTo("Fizz"));
         }
 
         [Test]
-        public void Show_buzz_if_multiple_of_5()
+        public void FizzBuzz_MultipleOf5_ReturnsBuzz()
         {
             Assert.That(fizzBuzz.DoFizzBuzz(5), Is.EqualTo("Buzz"));
         }
 
         [Test]
-        public void Show_fizzbuzz_if_multiple_of_both_3_and_5()
+        public void FizzBuzz_MultipleOf3And5_ReturnsFizzBuzz()
         {
             Assert.That(fizzBuzz.DoFizzBuzz(15), Is.EqualTo("FizzBuzz"));
         }
